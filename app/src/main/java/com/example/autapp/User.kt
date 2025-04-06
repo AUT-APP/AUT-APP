321ef153671b10dc15b4f983114f806cde1b1042
Rayyan Abzal <rayyanabzal@gmail.com>
2025-04-06 13:46:00 +1200

    feat: add User class
    Introduced a new User class, supports fields like id, name, and email with basic validation. Which also has an Open class meaning it can be inherited.

---
+++ app/src/main/java/com/example/autapp/User.kt
@@ -0,0 +1,25 @@
+package com.example.autapp
+
+open class User(
+    var firstName: String,
+    var lastName: String,
+    var id: Int,
+    var role: String,
+    var username: String,
+    var password: String
+) {
+    fun getFirstName(): String = firstName
+    fun getLastName(): String = lastName
+    fun setFirstName(name: String) { firstName = name }
+    fun setLastName(name: String) { lastName = name }
+
+    fun getId(): Int = id
+    fun setId(id: Int) { this.id = id }
+
+    fun getRole(): String = role
+    fun setRole(role: String) { this.role = role }
+
+    fun getUsername(): String = username
+    fun getPassword(): String = password
+    fun setPassword(password: String) { this.password = password }
+}
