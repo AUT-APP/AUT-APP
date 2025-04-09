1a2eea43f5d6454f2328e4dee7ae36597f0f9253
Rayyan Abzal <rayyanabzal@gmail.com>
2025-04-09 21:00:34 +1200

    feat: Course Repository Class - Created Course Repo Class
    - Matched CourseDAO methods
    - Used for managing course data
    - Is an intermediary for CourseDAO

--- app/src/main/java/com/example/autapp/data/repository/CourseRepository.kt
+++ app/src/main/java/com/example/autapp/data/repository/CourseRepository.kt
@@ -1,4 +1,31 @@
 package com.example.autapp.data.repository
 
-class CourseRepository {
+import com.example.autapp.data.dao.CourseDao
+import com.example.autapp.data.models.Course
+
+class CourseRepository(private val courseDao: CourseDao) {
+
+    suspend fun insertCourse(course: Course) {
+        courseDao.insertCourse(course)
+    }
+
+    suspend fun getCourseById(courseId: Int): Course? {
+        return courseDao.getCourseById(courseId)
+    }
+
+    suspend fun getCourseByName(name: String): Course? {
+        return courseDao.getCourseByName(name)
+    }
+
+    suspend fun getAllCourses(): List<Course> {
+        return courseDao.getAllCourses()
+    }
+
+    suspend fun deleteCourse(course: Course) {
+        courseDao.deleteCourse(course)
+    }
+
+    suspend fun updateCourse(course: Course) {
+        courseDao.updateCourse(course)
+    }
 }
