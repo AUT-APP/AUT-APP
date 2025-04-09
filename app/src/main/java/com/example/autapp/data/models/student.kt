905a7adfdbe39e6d7df340f0bfab906840e64ec2
Rayyan Abzal <rayyanabzal@gmail.com>
2025-04-09 20:44:30 +1200

    refactor: Student Class Refactored - Refactored Student Class
    - Implemented Room DB
    - Assigned a PK to studentId
    - removed get methods as not used in kotlin (built in)
    - Created a student_table db table @entity
    - Created a toString() method for displaying Student info

--- app/src/main/java/com/example/autapp/data/models/student.kt
+++ app/src/main/java/com/example/autapp/data/models/student.kt
@@ -1,21 +1,23 @@
-package com.example.autapp.com.example.autapp.data.models
+package com.example.autapp.data.models
 
-import java.time.LocalDate
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "student_table")
 class Student(
-    var firstName: String,
-    var lastName: String,
-    var id: Int,
-    var role: String,
-    var username: String,
-    var password: String,
-    var studentId: Int,
+    firstName: String,
+    lastName: String,
+    id: Int = 0,
+    username: String,
+    password: String,
+    @PrimaryKey var studentId: Int,
     var enrollmentDate: String,
     var major: String,
     var yearOfStudy: Int,
-    var gpa: Double,
-    var courseList: List<String>
-): User(firstName, lastName, id, role, password) {
-    fun getStudentId(): Int = studentId
-    fun getRole(): Int = studentId
+    var gpa: Double
+) : User(firstName, lastName, id, "Student", username, password) {
+    override fun toString(): String {
+        return "Student(firstName='$firstName', lastName='$lastName', id=$id, role='$role', username='$username', password='$password', " +
+                "studentId=$studentId, enrollmentDate='$enrollmentDate', major='$major', yearOfStudy=$yearOfStudy, gpa=$gpa)"
+    }
 }
