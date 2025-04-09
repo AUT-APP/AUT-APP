95bebfd55a004c5aa875a7856a8c7328d6270d01
Rayyan Abzal <rayyanabzal@gmail.com>
2025-04-09 20:58:45 +1200

    feat: Course DAO Class - Created Course DAO Class
    - CRUD operations for the course_table
    - All methods from our class diagram
    - Manages the course data

---
+++ app/src/main/java/com/example/autapp/data/dao/CourseDao.kt
@@ -0,0 +1,25 @@
+package com.example.autapp.data.dao
+
+import androidx.room.*
+import com.example.autapp.data.models.Course
+
+@Dao
+interface CourseDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCourse(course: Course)
+
+    @Query("SELECT * FROM course_table WHERE name = :name")
+    suspend fun getCourseByName(name: String): Course?
+
+    @Query("SELECT * FROM course_table WHERE courseId = :courseId")
+    suspend fun getCourseById(courseId: Int): Course?
+
+    @Query("SELECT * FROM course_table")
+    suspend fun getAllCourses(): List<Course>
+
+    @Delete
+    suspend fun deleteCourse(course: Course)
+
+    @Update
+    suspend fun updateCourse(course: Course)
+}
