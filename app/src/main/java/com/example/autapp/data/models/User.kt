d2adfcdb25f834e22448ac27e26577dc215c10b9
Rayyan Abzal <rayyanabzal@gmail.com>
2025-04-09 20:39:22 +1200

    refactor: User Class - Refactored User Class
    - Implemented room db
    - Assigned PK
    - Created user_table @entity
    - Created toString() Function
    - Removed get and set methods as unused and not needed in kotlin as it's built in

--- app/src/main/java/com/example/autapp/data/models/User.kt
+++ app/src/main/java/com/example/autapp/data/models/User.kt
@@ -1,25 +1,19 @@
-package com.example.autapp.com.example.autapp.data.models
+package com.example.autapp.data.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
 open class User(
     var firstName: String,
     var lastName: String,
-    var id: Int,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
     var role: String,
     var username: String,
     var password: String
 ) {
-    fun getFirstName(): String = firstName
-    fun getLastName(): String = lastName
-    fun setFirstName(name: String) { firstName = name }
-    fun setLastName(name: String) { lastName = name }
-
-    fun getId(): Int = id
-    fun setId(id: Int) { this.id = id }
-
-    fun getRole(): String = role
-    fun setRole(role: String) { this.role = role }
-
-    fun getUsername(): String = username
-    fun getPassword(): String = password
-    fun setPassword(password: String) { this.password = password }
+    override fun toString(): String {
+        return "User(firstName='$firstName', lastName='$lastName', id=$id, role='$role', username='$username', password='$password')"
+    }
 }
