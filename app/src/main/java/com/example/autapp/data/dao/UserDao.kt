9a1a927b9090d62d43ffa0479c74548d0f62d169
Rayyan Abzal <rayyanabzal@gmail.com>
2025-04-09 20:41:04 +1200

    feat: User DAO - Created User DAO Interface
    - Created a DAO interface which gives access to methods that have access to and interact with our database

---
+++ app/src/main/java/com/example/autapp/data/dao/UserDao.kt
@@ -0,0 +1,28 @@
+package com.example.autapp.data.dao
+
+import androidx.room.*
+import com.example.autapp.data.models.User
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: User)
+
+    @Query("SELECT * FROM user_table WHERE username = :username")
+    suspend fun getUserByUsername(username: String): User?
+
+    @Query("SELECT * FROM user_table WHERE id = :id")
+    suspend fun getUserById(id: Int): User?
+
+    @Query("SELECT * FROM user_table")
+    suspend fun getAllUsers(): List<User>
+
+    @Delete
+    suspend fun deleteUser(user: User)
+
+    @Update
+    suspend fun updateUser(user: User)
+
+    @Query("SELECT * FROM user_table WHERE username = :username AND password = :password")
+    suspend fun checkUser(username: String, password: String): User?
+}
