5906a3e3d41b6763bba3c3647f75dadd90086012
Rayyan Abzal <rayyanabzal@gmail.com>
2025-04-13 12:46:14 +1200

    feat: Converter Class  - Implemented Date to Long and Long to Date

--- app/src/main/java/com/example/autapp/data/database/Converters.kt
+++ app/src/main/java/com/example/autapp/data/database/Converters.kt
@@ -3,6 +3,7 @@ package com.example.autapp.data.database
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.util.Date
 
 class Converters {
     @TypeConverter
@@ -14,5 +15,15 @@ class Converters {
     fun toStringList(json: String?): List<String>? {
         val type = object : TypeToken<List<String>>() {}.type
         return Gson().fromJson(json, type)
+    }
+
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun toDate(timestamp: Long?): Date? {
+        return timestamp?.let { Date(it) }
     }
 }
